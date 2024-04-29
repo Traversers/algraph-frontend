1b3023d6128c966a5ce782a2a201fae08d4c5c14
Yoav Friedman <117902801+yoavf11@users.noreply.github.com>
2024-04-29 23:34:52 +0300

    implemented first PR suggestions

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppBar from "./components/AppBar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import CreateGraph from "./components/CreateGraph/CreateGraph";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="Signup" element={<Signup />} />
+        <Route path="Create" element={<CreateGraph />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

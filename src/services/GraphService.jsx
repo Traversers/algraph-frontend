4b26cfe27aa581b6a06618171288439297ff79df
noa-vinkler <noavinkler@gmail.com>
2024-05-01 17:33:16 +0300

    Graph service template

---
+++ src/services/GraphService.jsx
@@ -0,0 +1,16 @@
+const createGraph= async (graphData,user) => {
+    
+};
+ const getGraph = async (id) => {
+
+ }
+ const setGraph = async (id,graphData) => {
+
+ }
+ const deleteGraph = async (id) => {
+
+ }
+ const getPreviewGraph = async (id) => {
+    
+ }
+ 

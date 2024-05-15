acfa3a0e58faee81ccc1c54c071f155c6fcc722a
Yoav Friedman <117902801+yoavf11@users.noreply.github.com>
2024-05-15 15:28:17 +0300

    Added the updated constants file

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -17,6 +17,34 @@ export const CREATE_GRAPH_BUTTONS_TEXTS = {
   CREATE_GRAPH: "Create Graph",
 };
 
+export const LOGIN_ALERTS = {
+  GENERIC_ERROR: "Something went wrong. Please try again.",
+  INVALID_CREDENTIALS: "Invalid email or password.",
+  USERNAME_REQUIRED: "Please input your username!",
+  PASSWORD_REQUIRED: "Please input your password!",
+  LOG_IN: "Log In",
+  NO_ACCOUNT: "Don't Have an account?",
+  REGISTER: "Register now!",
+};
+
+export const SIGNUP_ALERTS = {
+  GENERIC_ERROR: "Something went wrong. Please try again.",
+  MISSING_FIELDS: "Please fill all the fields.",
+  EMAIL_REQUIRED: "Please input your email!",
+  EMAIL_INVALID: "Please input a valid email!",
+  NAME_REQUIRED: "Please input your name!",
+  PASSWORD_REQUIRED: "Please input your password!",
+  PASSWORD_CONFIRM_REQUIRED: "Please confirm your password!",
+  CONFIRM_PASSWORD: "Confirm Password",
+  PASSWORD_LENGTH: "Password must be at least 6 characters long.",
+  PASSWORDS_DONT_MATCH: "Passwords do not match!",
+  USER_EXISTS: "User already exists.",
+  REGISTER: "Register",
+  HAVE_ACCOUNT: "Already have an account?",
+  LOGIN: "Log In!",
+
+};
+
 export const GRAPH_PREVIEW = {};
 
 export const BACKEND_URL = "http://localhost:8000";
